aaf1a13c9b826fb47cd0058e3af39c6919252a3f
RangaReddy12 <pranga2010@gmail.com>
2026-02-17 12:03:18 +0530

    New Method added

---
+++ src/test/java/feb17/Demo.java
@@ -0,0 +1,23 @@
+package feb17;
+
+public class Demo {
+public void add()
+{
+	System.out.println("Add");
+}
+public void sub()
+{
+	System.out.println("Add");
+}
+
+public void mul()
+{
+	System.out.println("Add");
+}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
