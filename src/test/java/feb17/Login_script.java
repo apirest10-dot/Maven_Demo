b42d1e9327a18e8a6c6783747a57f23190d8f119
RangaReddy12 <pranga2010@gmail.com>
2026-02-17 12:13:55 +0530

    New Class Created

---
+++ src/test/java/feb17/Login_script.java
@@ -0,0 +1,16 @@
+package feb17;
+
+public class Login_script {
+public void adminLogin()
+{
+	System.out.println("Login");
+}
+public void customer()
+{
+	System.out.println("Login");
+}
+public void supplier()
+{
+	System.out.println("Login");
+}
+}
